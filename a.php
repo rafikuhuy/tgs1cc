009f6d8e79652de949b3ff74c44dbc612885b3cb
rafikuhuy <rafik.uhuy@yahoo.com>
2016-09-22 12:56:38 +0700

    Create a.php

---
+++ a.php
@@ -0,0 +1,29 @@
+<?php
+	echo 	"<head>
+			<title>Tugas 1 Cloud Computing</title>
+		</head>
+		<h2> Curiculum Vitae </h2>
+		<hr>
+		<table>
+			<tr>
+				<td>Nama</td>
+				<td>:</td>
+				<td>Rafik Ibnu Alwan</td>
+			</tr>
+			<tr>
+				<td>NIM</td>
+				<td>:</td>
+				<td>165410199</td>
+			</tr>
+			<tr>
+				<td>Alamat</td>
+				<td>:</td>
+				<td>Jimatan, Jatirejo, Lendah, Kulon Progo, Yogyakarta</td>
+			</tr>
+			<tr>
+				<td>TTL</td>
+				<td>:</td>
+				<td>Kulon Progo / 20 Mei 1989</td>
+			</tr>
+		</table>";
+?>
